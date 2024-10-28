079e9e4e9d0ba04116a77c4afa7d1650bab4f238
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2024-10-28 20:46:12 +0530

    Update UserSchema.js

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    role: { type: String, enum: ["admin", "user"], default: "user" },
+   
   },
   { timestamps: true }
 );
